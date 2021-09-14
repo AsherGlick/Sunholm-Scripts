87bf32225c0f5dafc981d8dccc9049ce1d2813a1
Asher Glick <asher.glick@gmail.com>
2021-09-14 01:48:43 -0500

    updating spell display list to include more info and some formatting

--- random_spell.py
+++ random_spell.py
@@ -1,18 +1,42 @@
 import json
+import datetime
 import random
 
-with open("spells_20210508.json") as f:
+today = datetime.datetime.now()
+date = today.strftime('%Y/%m/%d')
+
+
+
+
+with open("spells_20210914.json") as f:
     spells = json.load(f)
 
-random_spells = random.sample(spells, 3)
+# random.seed(date)
+random_spells = random.sample(spells, 6)
 
-max_length = 0
-for random_spell in random_spells:
-    if max_length < len(random_spell["text"]):
-        max_length = len(random_spell["text"])
+cost_lookup = {
+    "Cantrip": 10,
+    "1st": 60,
+    "2nd": 120,
+    "3rd": 200,
+    "4th": 320,
+    "5th": 640,
+    "6th": 1280,
+    "7th": 2560,
+    "8th": 5120,
+    "9th": 10240,
+}
 
+
+random_spells.sort(key=lambda x: str(cost_lookup[x["level"]]).zfill(5) + x["text"])
+
+
+print("__{padding}**Legend 03's Spells** - {date}{padding}__".format(date=date, padding=" "*16))
 for random_spell in random_spells:
-    print("{} : https://www.dndbeyond.com{}".format(
-        random_spell["text"].ljust(max_length, ' '),
-        random_spell["link"]
+    print("\n**{name}** - <https://www.dndbeyond.com{link}>\n{cost}Gp - {level} Level - {school}".format(
+        name=random_spell["text"],#.ljust(max_length, ' '),
+        level=random_spell["level"],
+        cost=cost_lookup[random_spell["level"]],
+        link=random_spell["link"],
+        school=random_spell["school"].capitalize(),
     ))
